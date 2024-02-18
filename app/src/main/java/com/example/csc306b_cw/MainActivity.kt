f4c8cfdb7e364f7df3f34fd93165f0a8e8d1657c
JakeDaDawg <42595576+MaxChung0529@users.noreply.github.com>
2024-02-18 16:39:52 +0000

    Replaced the greeting view with main view

--- app/src/main/java/com/example/csc306b_cw/MainActivity.kt
+++ app/src/main/java/com/example/csc306b_cw/MainActivity.kt
@@ -6,29 +6,42 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.csc306b_cw.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val baldurGreet = findViewById<TextView>(R.id.greetTexts)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main_view)
+        setContentView(binding.root)
+        replaceFragment(Logs())
 
-        baldurGreet.text = getText(R.string.askName)
+        binding.bottomNavMenu.setOnItemSelectedListener {
+
+            when (it.itemId) {
+
+                R.id.logs -> replaceFragment(Logs())
+                R.id.goals -> replaceFragment(Goals())
+                R.id.stopwatch -> replaceFragment(Stopwatch())
+                R.id.settings -> replaceFragment(Settings())
+
+                else ->{
+
+                }
+            }
+            true
+        }
     }
 
-    fun updateText(view : View) {
-        val baldurGreet = findViewById<TextView>(R.id.greetTexts)
-        val enteredName = findViewById<EditText>(R.id.nameInput)
-
-        baldurGreet.text = getString(R.string.reply1, enteredName.text)
-
-        baldurGreet.textSize = 20.0F
-        enteredName.visibility = View.INVISIBLE
-
-        val updateBtn = findViewById<Button>(R.id.update)
-        updateBtn.visibility = View.INVISIBLE
-
-
+    private fun replaceFragment(fragment : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
